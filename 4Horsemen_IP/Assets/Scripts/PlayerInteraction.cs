b0c13f9111ea54d6770db0feab633d25e2d5dcc1
ET-NP-ACC <103923257+ET-NP-ACC@users.noreply.github.com>
2023-02-08 22:23:14 +0800

    Added Room 3

---
+++ 4Horsemen_IP/Assets/Scripts/PlayerInteraction.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Procedure
+// 1. Player Spawns in front of the entrance gate (which is barricaded)
+
+// 2. The painting with the clue is at the fireplace. Player must
+// collect 8 tokens around the room and placed them in the correct position
+
+// 3. The roman numerals on the painting indicates the positions where the tokens should place.
+
+// 4. Once the tokens are placed in correct position, the fire in the fireplace will turn off,
+// the password will reveal (the password is 4780)
+
+// 5. After entering the password in the treasure chest (located in front of the exit gate), the chest will open.
+
+// 6. Grab the handle inside the treasure chest, put it in the handle slot, pull down the handle to
+// open the door and exit.
+
+public class PlayerInteraction : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
