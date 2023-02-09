008d8e8ccb9e783c80115b075e4e6640b94dcbee
abellsk <s10227813@connect.np.edu.sg>
2023-02-09 17:29:51 +0800

    gun, room3
    gun is now attached with shooting mechanic script, but not working,
    players are now able to walk around room 3 as well

---
+++ 4Horsemen_IP/Assets/Scripts/Gun.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    public Transform bulletOrigin;
+    public GameObject bulletPrefab;
+
+
+    public void Fire()
+    {
+        // Spawn a new bullet at the position of bulletOrgin
+        GameObject newBullet = Instantiate(bulletPrefab, bulletOrigin.position, bulletOrigin.rotation);
+
+        newBullet.GetComponent<Rigidbody>().AddForce(bulletOrigin.forward * 50f);
+    }
+
+}
