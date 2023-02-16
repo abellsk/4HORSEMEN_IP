d1e429a060849f8463d94174496df963d8834586
abellsk <s10227813@connect.np.edu.sg>
2023-02-16 14:20:28 +0800

    targets have function once shot
    scorecounter will go up but not linked yet
    targets will disappear for now

--- 4Horsemen_IP/Assets/Scripts/BulletTarget.cs
+++ 4Horsemen_IP/Assets/Scripts/BulletTarget.cs
@@ -1,18 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class BulletTarget : MonoBehaviour
 {
     public GameObject targetShot;
+    public GameObject targetShot2;
     int targetDown = 0;
     bool allDead = false;
+    public TextMeshProUGUI playerScoreText1; //show current score in screen 1
+    public TextMeshProUGUI playerScoreText2; //show current score in screen 2
+    public TextMeshProUGUI playerScoreText3; //show current score in screen 3
+    public TextMeshProUGUI playerScoreText4; //show current score in screen 4
 
     void OnCollisionEnter(Collision collision) {
         if (collision.gameObject.tag == "Target")
         {
             Debug.Log("Target has been hit");
+            Destroy(GameObject.FindWithTag("Target"));
             targetDown++;
+            playerScoreText1.text = targetDown.ToString();
+            playerScoreText2.text = targetDown.ToString();
+            playerScoreText3.text = targetDown.ToString();
+            playerScoreText4.text = targetDown.ToString();
         }
         if (collision.gameObject.tag == "Target1")
         {
--- 4Horsemen_IP/Assets/Scripts/Gun.cs
+++ 4Horsemen_IP/Assets/Scripts/Gun.cs
@@ -6,16 +6,14 @@ public class Gun : MonoBehaviour
 {
     public Transform bulletOrigin;
     public GameObject bulletPrefab;
-    public GameObject targetShot;
 
 
     public void Fire()
     {
-        Debug.Log("hello");
         // Spawn a new bullet at the position of bulletOrgin
         GameObject newBullet = Instantiate(bulletPrefab, bulletOrigin.position, bulletOrigin.rotation);
 
-        newBullet.GetComponent<Rigidbody>().AddForce(bulletOrigin.forward * 50f);
+        newBullet.GetComponent<Rigidbody>().AddForce(bulletOrigin.forward * 150f);
     } 
 
 }
